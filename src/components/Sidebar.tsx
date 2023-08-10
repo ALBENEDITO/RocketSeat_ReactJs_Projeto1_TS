e62570f8242c47552fd71d2e5d34cfde73c30538
Allan Benedito <allan.benedito@gmail.com>
2023-08-10 15:29:30 -0300

    TypeScript no ReactJS

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/albenedito.png" />
+        <strong>Allan Benedito</strong>
+        <span>Web Developer jr</span>
+      </div>
+
+      <footer>
+      <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
